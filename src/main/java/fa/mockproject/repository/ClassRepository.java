6532273b5a5efa414f01de90dc500428034ed306
QuyNguyen <quyquy97@gmail.com>
2022-06-11 20:41:54 +0700

    Update Model

--- src/main/java/fa/mockproject/repository/ClassRepository.java
+++
@@ -1,8 +0,0 @@
-package fa.mockproject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import fa.mockproject.entity.Class;
-
-public interface ClassRepository extends JpaRepository<Class, Long>{
-
-}
