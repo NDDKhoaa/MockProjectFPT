438302273ce095fae7bcfb064c780ba356cbf934
Eziovan-git <thuyvan.pham0105@gmail.com>
2022-06-17 19:20:10 +0700

    update entity, service, add controller

---
+++ src/main/java/fa/mockproject/controller/TrainerController.java
@@ -0,0 +1,20 @@
+package fa.mockproject.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import fa.mockproject.service.TrainerService;
+
+@Controller
+public class TrainerController {
+	@Autowired
+	private TrainerService trainerService;
+	
+	@GetMapping("/showTrainerList")
+	public String showTrainerList(Model model) {
+		model.addAttribute("trainerList", trainerService.getAllTrainers());
+		return "trainerList";
+	}
+}
