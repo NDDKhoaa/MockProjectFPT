354755dfe5a0a6525dfd49fcf92c003638f31cdb
QuyNguyen <quyquy97@gmail.com>
2022-06-17 19:59:35 +0700

    Delete user, userDetail, userService class. Comment dependency Spring Boot Security in POM

--- src/main/java/fa/mockproject/security/WebSecurityConfig.java
+++ src/main/java/fa/mockproject/security/WebSecurityConfig.java
@@ -12,8 +12,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Qualifier("userDetailsServiceImpl")
