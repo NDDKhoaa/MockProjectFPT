5cc4ff923ed0bf4e6985f9e85b09ebf7d57db787
NDDKhoaa <nddkhoaa2408@gmail.com>
2022-06-20 19:52:10 +0700

    Add SQL values

---
+++ sql.sql
@@ -0,0 +1,60 @@
+INSERT INTO `mockproject`.`location` (`location_id`, `location_name`) VALUES ('HCM', 'Ho Chi Minh');
+INSERT INTO `mockproject`.`location` (`location_id`, `location_name`) VALUES ('HN', 'Ha Noi');
+INSERT INTO `mockproject`.`location` (`location_id`, `location_name`) VALUES ('DN', 'Da Nang');
+INSERT INTO `mockproject`.`location` (`location_id`, `location_name`) VALUES ('NT', 'Nha Trang');
+INSERT INTO `mockproject`.`location` (`location_id`, `location_name`) VALUES ('VT', 'Vung Tau');
+INSERT INTO `mockproject`.`location` (`location_id`, `location_name`) VALUES ('CT', 'Can Tho');
+
+
+INSERT INTO `mockproject`.`faculty` (`faculty_id`, `faculty_name`) VALUES ('FL', 'Foreign Language');
+INSERT INTO `mockproject`.`faculty` (`faculty_id`, `faculty_name`) VALUES ('MKT', 'Marketting');
+INSERT INTO `mockproject`.`faculty` (`faculty_id`, `faculty_name`) VALUES ('BA', 'Business Administration');
+INSERT INTO `mockproject`.`faculty` (`faculty_id`, `faculty_name`) VALUES ('TESOL', 'Teaching English to Speakers of Other Languages');
+INSERT INTO `mockproject`.`faculty` (`faculty_id`, `faculty_name`) VALUES ('BM', 'Brand Management');
+INSERT INTO `mockproject`.`faculty` (`faculty_id`, `faculty_name`) VALUES ('BE', 'Biomedical Engineering');
+INSERT INTO `mockproject`.`faculty` (`faculty_id`, `faculty_name`) VALUES ('FB', 'Finance and Banking');
+INSERT INTO `mockproject`.`faculty` (`faculty_id`, `faculty_name`) VALUES ('HRM', 'Human Resource Management');
+INSERT INTO `mockproject`.`faculty` (`faculty_id`, `faculty_name`) VALUES ('IT', 'Information Technology');
+
+
+INSERT INTO `mockproject`.`channel` (`channel_id`, `channel_name`) VALUES ('MAIL', 'Email Advertise');
+INSERT INTO `mockproject`.`channel` (`channel_id`, `channel_name`) VALUES ('WEB', 'Website Advertise');
+INSERT INTO `mockproject`.`channel` (`channel_id`, `channel_name`) VALUES ('FBOOK', 'Facebook Advertise');
+INSERT INTO `mockproject`.`channel` (`channel_id`, `channel_name`) VALUES ('INSTA', 'Instagram Advertise');
+INSERT INTO `mockproject`.`channel` (`channel_id`, `channel_name`) VALUES ('TV', 'Television Advertise');
+INSERT INTO `mockproject`.`channel` (`channel_id`, `channel_name`) VALUES ('NEWS', 'Newspaper Advertise');
+
+
+INSERT INTO `mockproject`.`university` (`university_id`, `university_name`) VALUES ('VLU', 'Van Lang University');
+INSERT INTO `mockproject`.`university` (`university_id`, `university_name`) VALUES ('HSU', 'Hoa Sen University');
+INSERT INTO `mockproject`.`university` (`university_id`, `university_name`) VALUES ('SIU', 'Saigon Internation University');
+INSERT INTO `mockproject`.`university` (`university_id`, `university_name`) VALUES ('VNU', 'Vietnam National University');
+INSERT INTO `mockproject`.`university` (`university_id`, `university_name`) VALUES ('NEU', 'National Economics University');
+INSERT INTO `mockproject`.`university` (`university_id`, `university_name`) VALUES ('FTU', 'Foreign Trade University');
+INSERT INTO `mockproject`.`university` (`university_id`, `university_name`) VALUES ('USSH', 'University of Social Science & Humanities');
+INSERT INTO `mockproject`.`university` (`university_id`, `university_name`) VALUES ('HNUE', 'Hanoi National University of Education');
+INSERT INTO `mockproject`.`university` (`university_id`, `university_name`) VALUES ('UAF', 'University of Agriculture & Forestry');
+INSERT INTO `mockproject`.`university` (`university_id`, `university_name`) VALUES ('FPT', 'FPT University');
+
+
+INSERT INTO `mockproject`.`skill` (`skill_id`, `skill_name`) VALUES ('Java', 'Java ');
+INSERT INTO `mockproject`.`skill` (`skill_id`, `skill_name`) VALUES ('SQL', 'SQL');
+INSERT INTO `mockproject`.`skill` (`skill_id`, `skill_name`) VALUES ('HTML/CSS', 'HTML/CSS');
+INSERT INTO `mockproject`.`skill` (`skill_id`, `skill_name`) VALUES ('C', 'C');
+INSERT INTO `mockproject`.`skill` (`skill_id`, `skill_name`) VALUES ('C#', 'C#');
+INSERT INTO `mockproject`.`skill` (`skill_id`, `skill_name`) VALUES ('Anguluar', 'Anguluar');
+INSERT INTO `mockproject`.`skill` (`skill_id`, `skill_name`) VALUES ('ReactJS', 'ReactJS');
+INSERT INTO `mockproject`.`skill` (`skill_id`, `skill_name`) VALUES ('JavaScript', 'JavaScript');
+INSERT INTO `mockproject`.`skill` (`skill_id`, `skill_name`) VALUES ('Python', 'Python');
+
+
+INSERT INTO `mockproject`.`profile_status` (`profile_status_id`, `status`) VALUES ('New', 'New');
+INSERT INTO `mockproject`.`profile_status` (`profile_status_id`, `status`) VALUES ('TP', 'Test - Pass');
+INSERT INTO `mockproject`.`profile_status` (`profile_status_id`, `status`) VALUES ('TF', 'Test - Failed');
+INSERT INTO `mockproject`.`profile_status` (`profile_status_id`, `status`) VALUES ('IP', 'Interview - Pass');
+INSERT INTO `mockproject`.`profile_status` (`profile_status_id`, `status`) VALUES ('IF', 'Interview - Failed');
+
+
+INSERT INTO `mockproject`.`profile_type` (`profile_status_id`, `type`) VALUES ('Candidate', 'Candidate');
+INSERT INTO `mockproject`.`profile_type` (`profile_status_id`, `type`) VALUES ('Trainee', 'Trainee');
+
