b63fec469b6e8ce793200437aa7b50ca366145da
linhhm1993 <linh.hm1993@gmail.com>
2022-06-29 18:55:47 +0700

    - Remove security

--- src/main/java/fa/mockproject/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package fa.mockproject.repository;
-
-import fa.mockproject.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-     User findByEmail(String user);
-     User findByUser(User userId);
-}
